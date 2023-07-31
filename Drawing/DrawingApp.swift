9f3d8c52ba34430c7c1193b0f5e2306650497a67
Alex Bonder <lexbonder@gmail.com>
2023-07-31 08:13:57 -0400

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Alex Bonder on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
